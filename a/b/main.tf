bbbe273f7613d9965228d9f8c197c9024bed4779
Curtis Hodak <curtish@spacelift.com>
2025-06-26 10:18:47 -0400

    remove b

--- a/b/main.tf
+++ a/b/main.tf
@@ -12,16 +12,16 @@
 #   }
 # }
 
-resource "aws_instance" "onboarding2" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  key_name      = "chkey1"
+# resource "aws_instance" "onboarding2" {
+#   ami           = var.instance_ami
+#   instance_type = var.instance_type
+#   key_name      = "chkey1"
 
-  root_block_device {
-    volume_size = 20
-  }
+#   root_block_device {
+#     volume_size = 20
+#   }
 
-  tags = {
-    test = "New Changes1"
-  }
-}
+#   tags = {
+#     test = "New Changes1"
+#   }
+# }
