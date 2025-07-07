13bdcf4a1cfd260ef7bd5433da9ffd0098414f4b
Curtis Hodak <curtish@spacelift.com>
2025-07-07 14:55:28 -0400

    make a root server

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "rootserver" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = "chkey1"
+
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    name = "rootserver"
+  }
+}
+
+# resource "aws_instance" "onboarding2" {
+#   ami           = var.instance_ami
+#   instance_type = var.instance_type
+#   key_name      = "chkey1"
+
+#   root_block_device {
+#     volume_size = 20
+#   }
+
+#   tags = {
+#     test = "New Changes1"
+#   }
+# }
