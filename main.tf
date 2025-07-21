4171a32cdafdf10a8dabba803ecfbbea8c1249ef
Curtis Hodak <curtish@spacelift.com>
2025-07-21 14:00:18 -0400

    open and close this

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-# resource "aws_instance" "rootserver" {
-#   ami           = var.instance_ami
-#   instance_type = var.instance_type
-#   key_name      = "chkey1"
+resource "aws_instance" "rootserver" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = "chkey1"
 
-#   root_block_device {
-#     volume_size = 20
-#   }
+  root_block_device {
+    volume_size = 20
+  }
 
-#   tags = {
-#     name = "rootserver"
-#   }
-# }
+  tags = {
+    name = "rootserver"
+  }
+}
 
 # resource "aws_instance" "onboarding2" {
 #   ami           = var.instance_ami
