96a9a8a4a0c480c0e2eb4e29f241b9ad7b3e71e1
Curtis Hodak <curtish@spacelift.com>
2025-07-21 13:07:26 -0400

    newest different hash

--- main.tf
+++ main.tf
@@ -12,18 +12,18 @@ resource "aws_instance" "rootserver" {
   }
 }
 
-# resource "aws_instance" "onboarding2" {
-#   ami           = var.instance_ami
-#   instance_type = var.instance_type
-#   key_name      = "chkey1"
+resource "aws_instance" "onboarding2" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = "chkey1"
 
-#   root_block_device {
-#     volume_size = 20
-#   }
+  root_block_device {
+    volume_size = 20
+  }
 
-#   tags = {
-#     test = "New Changes1"
-#   }
-# }
+  tags = {
+    test = "New Changes1"
+  }
+}
 
-#####fjdkls;ajkfld;jfkl;sadjfkl;asjkl;
+#####
